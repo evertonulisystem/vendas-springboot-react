e78d9aa75c833430d6f46da6a94d55ad1bd25ac2
Everton Ulisses <everton.mendonca@cassi.com.br>
2022-11-26 23:58:45 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/gotasdetecnologia/vendasspringbootreact/VendasSpringbootReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotasdetecnologia.vendasspringbootreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasSpringbootReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
